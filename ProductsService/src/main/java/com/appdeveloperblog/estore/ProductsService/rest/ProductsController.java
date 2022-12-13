7142d8a93300b13a900b875d2aa098197f857821
KhalilMinalang <minalang.khalil@gmail.com>
2022-12-13 19:18:21 +0800

    Completed: 3. Creating a Rest Controller class

---
+++ ProductsService/src/main/java/com/appdeveloperblog/estore/ProductsService/rest/ProductsController.java
@@ -0,0 +1,11 @@
+package com.appdeveloperblog.estore.ProductsService.rest;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+// @RestController is a convenience annotation that combines @Controller and @ResponseBody
+@RestController
+@RequestMapping("/products") // http://localhost:8080/products
+public class ProductsController {
+
+}
