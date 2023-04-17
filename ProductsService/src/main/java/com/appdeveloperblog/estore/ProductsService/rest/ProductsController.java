0a478fcf02ef61d6f2ef1e16cfefe3da4d2a8730
KhalilMinalang <minalang.khalil@gmail.com>
2023-04-17 13:06:51 +0800

    Completed: 5. Creating a new Command class

--- ProductsService/src/main/java/com/appdeveloperblog/estore/ProductsService/rest/ProductsController.java
+++ ProductsService/src/main/java/com/appdeveloperblog/estore/ProductsService/rest/ProductsController.java
@@ -1,5 +1,7 @@
 package com.appdeveloperblog.estore.ProductsService.rest;
 
+import java.util.UUID;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -9,6 +11,8 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.appdeveloperblog.estore.ProductsService.command.CreateProductCommand;
 
 // @RestController is a convenience annotation that combines @Controller and @ResponseBody
 @RestController
@@ -20,6 +24,14 @@ public class ProductsController {
 
   @PostMapping
   public String createProduct(@RequestBody CreateProductRestModel createProductRestModel) {
+
+    CreateProductCommand createProductCommand = CreateProductCommand.builder()
+        .price(createProductRestModel.getPrice())
+        .quantity(createProductRestModel.getQuantity())
+        .title(createProductRestModel.getTitle())
+        .productId(UUID.randomUUID().toString())
+        .build();
+
     return "HTTP POST Handled " + createProductRestModel.getTitle();
   }
 
